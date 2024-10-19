26407d8572f128f3668bf839a102047b1a0f78ff
Olexandr-L45D <litvinenko1978aleks@gmail.com>
2024-10-19 08:16:05 +0300

    updaet

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -3,7 +3,9 @@ import { readContacts } from "../utils/readContacts.js";
 
 export const countContacts = async () => {
     const contact = await readContacts();
-    return contact.lenght;
+    return contact.length;
 };
 
 console.log(await countContacts());
+
+//  В разі виконання скрипту count, у консоль має виводитись кількість контактів в масиві з файлу src / db / db.json, як це передбачено в завданні.Наразі повертається undefined https://prnt.sc/T4OLt0ugSB1Q
